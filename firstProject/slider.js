eb39b89065e5d649b7340bec8caa1eeb04b6156d
Prikop3sm <900543@list.ru>
2024-03-20 16:10:24 +0300

    slider ended and drop down menu started

--- firstProject/slider.js
+++ firstProject/slider.js
@@ -5,45 +5,29 @@ const formWidth = form.getBoundingClientRect().width;
 const items = document.querySelectorAll(".customer__content-item");
 
 function flag(event) {
+  if (!event?.target?.classList.contains("slider__scroll")) return;
+
   document.addEventListener("pointermove", sliderMover);
+}
+
+function noFlag(event) {
+  document.removeEventListener("pointermove", sliderMover);
 }
 
 function sliderMover(event) {
   const target = event.target;
   event.preventDefault();
 
-  if (
-    target.classList.contains("slider__form") ||
-    target.classList.contains("slider__scroll")
-  ) {
-    console.log(formWidth);
+  event.clientX - formWidth + sliderWidth / 2 < 0
+    ? (slider.style.left = 0 + "px")
+    : event.clientX - formWidth + sliderWidth / 2 > 699 - sliderWidth
+    ? (slider.style.left = 699 - sliderWidth + "px")
+    : (slider.style.left = event.clientX - formWidth + sliderWidth / 2 + "px");
 
-    event.clientX - formWidth + sliderWidth / 2 < 0
-      ? (slider.style.left = 0 + "px")
-      : event.clientX - formWidth + sliderWidth / 2 > 699 - sliderWidth
-      ? (slider.style.left = 699 - sliderWidth + "px")
-      : (slider.style.left =
-          event.clientX - formWidth + sliderWidth / 2 + "px");
-
-    items.forEach((item) => {
-      item.style.right = slider.style.left.slice(0, -2) * 5.6 + "px";
-    });
-  }
-
-  // if (target.classList.contains("slider__scroll")) {
-  //   event.clientX - formWidth + sliderWidth / 2 < 0
-  //     ? (slider.style.left = 0 + "px")
-  //     : event.clientX - formWidth + sliderWidth / 2 > 699 - sliderWidth
-  //     ? (slider.style.left = 699 - sliderWidth + "px")
-  //     : (slider.style.left =
-  //         event.clientX - formWidth + sliderWidth / 2 + "px");
-
-  //   items.forEach((item) => {
-  //     item.style.right = slider.style.left.slice(0, -2) * 5.6 + "px";
-  //   });
-  // }
+  items.forEach((item) => {
+    item.style.right = slider.style.left.slice(0, -2) * 5.6 + "px";
+  });
 }
 
 document.addEventListener("pointerdown", flag);
-// document.addEventListener("pointermove", sliderMover);
-//document.addEventListener("pointermove", sliderMove);
+document.addEventListener("pointerup", noFlag);
